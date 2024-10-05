ed21e2e744ade0944c20189c22c55e2c5d6c6161
ast8705 <andtoskas1104@gmail.com>
2024-10-05 12:05:52 -0400

    added graph

--- src/simulation_strategies/pid_based_removal_strategy.py
+++ src/simulation_strategies/pid_based_removal_strategy.py
@@ -43,6 +43,7 @@ class PIDBasedRemovalStrategy(fl.server.strategy.FedAvg):
         self.kd = kd
         self.kp = kp
 
+        self.current_threshold = pid_threshold
         self.rounds_history = {}
 
 
@@ -111,13 +112,16 @@ class PIDBasedRemovalStrategy(fl.server.strategy.FedAvg):
 
         self.rounds_history[f'{self.current_round}']['client_info'] = {}
 
-
+        pids = []   
         for i, (client_proxy, _) in enumerate(results):
             client_id = client_proxy.cid
             curr_dist = normalized_distances[i][0]
 
             new_PID = self.calculate_pid(client_id, curr_dist)
             self.client_pids[client_id] = new_PID
+
+            if not client_id in self.removed_client_ids:
+                pids.append(new_PID)
 
             self.client_distances[client_id] = curr_dist
             curr_sum = self.client_distance_sums.get(client_id, 0)
@@ -142,6 +146,12 @@ class PIDBasedRemovalStrategy(fl.server.strategy.FedAvg):
                 f'Normalized Distance: {normalized_distances[i][0]} '
             )
 
+        pid_avg = np.mean(pids)
+        pid_std = np.std(pids)
+        self.rounds_history[f'{self.current_round}']['average'] = pid_avg
+        self.rounds_history[f'{self.current_round}']['standard_deviation'] = pid_std
+        self.current_threshold = pid_avg + (3*pid_std)
+        print(self.current_threshold, flush=True)
 
         return aggregated_parameters, aggregated_metrics
 
@@ -157,26 +167,26 @@ class PIDBasedRemovalStrategy(fl.server.strategy.FedAvg):
         # fetch the Trust and Reputation scores for all available clients
         client_pids = {client_id: self.client_pids.get(client_id, 0) for client_id in available_clients.keys()}
 
-        if self.remove_clients and len(self.removed_client_ids) < 2:
+        if self.remove_clients: # and len(self.removed_client_ids) < 2:
             # in the first round after warmup, remove the client with the highest PID
-            if self.current_round == self.begin_removing_from_round:
+            if self.current_round == self.begin_removing_from_round and False:
                 highest_pid_client = max(client_pids, key=client_pids.get)
                 logging.info(f"Removing client with highest PID: {highest_pid_client}")
                 # add this client to the removed_clients list
                 self.removed_client_ids.add(highest_pid_client)
-                self.rounds_history[f'{self.current_round}']['client_info'][f'client_{client_id}']['is_removed'] = True
+                self.rounds_history[f'{self.current_round}']['client_info'][f'client_{highest_pid_client}']['is_removed'] = True
 
             else:
                 # remove clients with PID lower than threshold.
-                for client_id, trust in client_pids.items():
-                    if trust > self.pid_threshold and client_id not in self.removed_client_ids:
+                for client_id, pid in client_pids.items():
+                    if pid > self.current_threshold and client_id not in self.removed_client_ids:
                         logging.info(f"Removing client with PID greater than Threshold: {client_id}")
                         # add this client to the removed_clients list
                         self.removed_client_ids.add(client_id)
                         self.rounds_history[f'{self.current_round}']['client_info'][f'client_{client_id}']['is_removed'] = True
 
 
-            logging.info(f"removed clients are : {self.removed_client_ids}")
+        logging.info(f"removed clients are : {self.removed_client_ids}")
 
         # select clients based on updated PID and available clients
         sorted_client_ids = sorted(client_pids, key=client_pids.get, reverse=True)
