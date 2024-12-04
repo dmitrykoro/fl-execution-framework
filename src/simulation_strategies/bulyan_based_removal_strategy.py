fix time calculation

src/simulation_strategies/bulyan_based_removal_strategy.py
@@ -38,9 +38,10 @@ class BulyanBasedRemovalStrategy(Bulyan):
             logging.warning("No clients available for aggregation after filtering removed clients.")
             return None, {}
 
+        distances = np.zeros((len(results), len(results)))
         time_start_calc = time.time_ns()
         # Calculate removal criteria scores for each client
-        scores = self._calculate_removal_criteria(aggregate_clients)
+        scores = self._calculate_removal_criteria(aggregate_clients, distances)
 
         # Select the top clients based on scores
         num_clients = len(aggregate_clients)
@@ -79,7 +80,8 @@ class BulyanBasedRemovalStrategy(Bulyan):
 
         return aggregated_parameters, {}
 
-    def _calculate_removal_criteria(self, results: List[Tuple[ClientProxy, FitRes]]) -> List[float]:
+    def _calculate_removal_criteria(self, results: List[Tuple[ClientProxy, FitRes]],
+                                    distances: List[float]) -> List[float]:
         """
         Calculate removal criteria scores for each client based on pairwise distances.
 
@@ -92,7 +94,6 @@ class BulyanBasedRemovalStrategy(Bulyan):
         param_data = [fl.common.parameters_to_ndarrays(fit_res.parameters) for _, fit_res in results]
         stacked_params = np.stack([np.concatenate([p.flatten() for p in params]) for params in param_data])
         num_clients = len(stacked_params)
-        distances = np.zeros((num_clients, num_clients))
         for i in range(num_clients):
             for j in range(i + 1, num_clients):
                 distances[i, j] = np.linalg.norm(stacked_params[i] - stacked_params[j])

src/simulation_strategies/multi_krum_based_removal_strategy.py
@@ -24,7 +24,8 @@ class MultiKrumBasedRemovalStrategy(Krum):
         self.current_round = 0
         self.rounds_history = {}
 
-    def _calculate_multi_krum_scores(self, results: List[Tuple[ClientProxy, FitRes]]) -> List[float]:
+    def _calculate_multi_krum_scores(self, results: List[Tuple[ClientProxy, FitRes]],
+                                     distances: List[float]) -> List[float]:
         """
         Calculate Multi-Krum scores based on the parameter differences between clients.
 
@@ -38,7 +39,6 @@ class MultiKrumBasedRemovalStrategy(Krum):
         flat_param_data = [np.concatenate([p.flatten() for p in params]) for params in param_data]
         param_data = flat_param_data
         num_clients = len(param_data)
-        distances = np.zeros((num_clients, num_clients))
 
         # Compute pairwise distances between clients' model updates
         for i in range(num_clients):
@@ -86,9 +86,10 @@ class MultiKrumBasedRemovalStrategy(Krum):
         scaler.fit(distances)
         normalized_distances = scaler.transform(distances)
 
+        distances = np.zeros((len(results), len(results)))
         time_start_calc = time.time_ns()
         
-        multi_krum_scores = self._calculate_multi_krum_scores(results)
+        multi_krum_scores = self._calculate_multi_krum_scores(results, distances)
         self.rounds_history[f'{self.current_round}']['client_info'] = {}
 
         # Select the top `num_krum_selections` clients based on Multi-Krum scores