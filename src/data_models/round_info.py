d0c891cb0bc5906516b28cbd972a89dc7a1175af
Dmitrii Korobeinikov <dk9148@rit.edu>
2025-02-14 16:40:29 -0500

    fixes2plots

--- src/data_models/round_info.py
+++ src/data_models/round_info.py
@@ -25,31 +25,42 @@ class RoundsInfo:
     removal_recall_history: List[float] = field(default_factory=list)
     removal_f1_history: List[float] = field(default_factory=list)
 
-    plottable_metrics = [
-        "score_calculation_time_nanos_history",
-        "aggregated_loss_history",
-        "average_accuracy_history",
-        "removal_accuracy_history",
-        "removal_precision_history",
-        "removal_recall_history",
-        "removal_f1_history",
-        "total_fp_and_fn_history",
-    ]
+    plottable_metrics = []
+    barable_metrics = []
+    savable_metrics = []
 
-    savable_metrics = [
-        "score_calculation_time_nanos_history",
-        "removal_threshold_history",
-        "aggregated_loss_history",
-        "average_accuracy_history",
-        "tp_history",
-        "tn_history",
-        "fp_history",
-        "fn_history",
-        "removal_accuracy_history",
-        "removal_precision_history",
-        "removal_recall_history",
-        "removal_f1_history",
-    ]
+    def __post_init__(self):
+        self.plottable_metrics = [
+            "score_calculation_time_nanos_history",
+            "aggregated_loss_history",
+            "average_accuracy_history",
+        ]
+
+        self.barable_metrics = [
+            "removal_accuracy_history",
+            "removal_precision_history",
+            "removal_recall_history",
+            "removal_f1_history",
+            "total_fp_and_fn_history",
+        ]
+
+        self.savable_metrics = [
+            "score_calculation_time_nanos_history",
+            "removal_threshold_history",
+            "aggregated_loss_history",
+            "average_accuracy_history",
+        ]
+        if self.simulation_strategy_config.remove_clients:
+            self.savable_metrics += [
+                "tp_history",
+                "tn_history",
+                "fp_history",
+                "fn_history",
+                "removal_accuracy_history",
+                "removal_precision_history",
+                "removal_recall_history",
+                "removal_f1_history",
+            ]
 
     def add_history_entry(
             self,
