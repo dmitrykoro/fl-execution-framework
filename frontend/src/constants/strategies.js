72a400037c24a84fda69eb2b8c093d5bdef301f2
AJ Barea <ajbareaa@gmail.com>
2025-10-09 17:04:24 -0400

    feat(constants): add aggregation strategy constants

---
+++ frontend/src/constants/strategies.js
@@ -0,0 +1,17 @@
+/**
+ * Aggregation Strategy Constants
+ */
+
+export const STRATEGIES = [
+  'fedavg',
+  'trust',
+  'pid',
+  'pid_scaled',
+  'pid_standardized',
+  'multi-krum',
+  'krum',
+  'multi-krum-based',
+  'trimmed_mean',
+  'rfa',
+  'bulyan',
+];
